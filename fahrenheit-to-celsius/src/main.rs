31fce7cfab6bdf2dc108433966e75d06470475b1
Kirk <klogold1@gmail.com>
2020-02-21 10:36:40 -0800

    Refactor read_float to read_temp

--- fahrenheit-to-celsius/src/main.rs
+++ fahrenheit-to-celsius/src/main.rs
@@ -1,11 +1,7 @@
 use std::io::stdin;
 
 fn main() {
-    println!("Enter a temperature in Fahrenheit:");
-
-    let mut temp_in_f: f32 = 0.0;
-
-    read_float(&mut temp_in_f);
+    let temp_in_f = read_temp();
 
     let temp_in_c = f_to_c(temp_in_f);
 
@@ -21,21 +17,25 @@ fn f_to_c(f: f32) -> f32 {
     (f - 32.0) * 5.0 / 9.0
 }
 
-fn read_float(f: &mut f32) {
-    let mut str_input = String::new();
+fn read_temp() -> f32 {
+    let mut str_input;
 
     loop {
+        println!("Enter a temperature in Fahrenheit: ");
+
+        str_input = String::new();
+
         stdin()
             .read_line(&mut str_input)
             .expect("That's not a valid temperature.");
 
         match str_input.trim().parse() {
-            Ok(num) => {
-                *f = num;
-                break;
-            }
+            Ok(num) => return num,
 
-            Err(_) => continue,
+            Err(_) => {
+                println!("\n{} is not a valid temperature. ", str_input.trim());
+                continue;
+            }
         };
     }
 }
